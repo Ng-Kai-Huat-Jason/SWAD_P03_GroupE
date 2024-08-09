b796a9fab07f42db5b8d84444a0a2f1be96596df
Jason Ng <S10262552@connect.np.edu.sg>
2024-08-09 20:20:33 +0800

    Added Inspection Report and Review Class

---
+++ SWAD_P03_GroupE_Classes/InspectionReport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_P03_GroupE_Classes
+{
+    public class InspectionReport
+    {
+        public string inspectionID { get; set; }
+        public string reportDetails { get; set; }
+        public DateTime inspectionDateTime { get; set; }
+        public string inspectorName { get; set; }
+        public string status { get; set; }
+
+        public InspectionReport()
+        {
+        }
+
+        public InspectionReport(string InspectionID, string ReportDetails, DateTime InspectionDateTime, string InspectorName, string Status)
+        {
+            inspectionID = InspectionID;
+            reportDetails = ReportDetails;
+            inspectionDateTime = InspectionDateTime;
+            inspectorName = InspectorName;
+            status = Status;
+        }
+
+
+    }
+}
---
+++ SWAD_P03_GroupE_Classes/Reviews.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_P03_GroupE_Classes
+{
+    public class Reviews
+    {
+        string reviewID { get; set; }
+        int starInt { get; set; }
+
+        string reviewDesc { get; set; }
+
+        public Reviews()
+        {
+        }
+
+        public Reviews(string ReviewID, int StarInt, string ReviewDesc)
+        {
+            reviewID = ReviewID;
+            starInt = StarInt;
+            reviewDesc = ReviewDesc;
+        }
+
+
+    }
+}
